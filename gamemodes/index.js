a8c66580029f3dccc2e55aa453caf233078ff27d
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-06 09:31:14 -0400

    Add files via upload

---
+++ gamemodes/index.js
@@ -0,0 +1,61 @@
+module.exports = {
+    Mode: require('./Mode'),
+    FFA: require('./FFA'),
+    Peaceful: require('./Peace'),
+    Teleports: require('./Teleports'),
+    Teams: require('./Teams'),
+    Experimental: require('./Experimental'),
+    Tournament: require('./Tournament'),
+    HungerGames: require('./HungerGames'),
+    Rainbow: require('./Rainbow'),
+    Debug: require('./Debug'),
+    Zombie: require('./Zombie'),
+    TeamZ: require('./TeamZ.js'),
+    TeamX: require('./TeamX.js'),
+    Peace: require('./Peace.js')
+};
+
+var get = function(id) {
+    var mode;
+    switch (id) {
+        case 1: // Teams
+            mode = new module.exports.Teams();
+            break;
+        case 2: // Experimental
+            mode = new module.exports.Experimental();
+            break;
+        case 5: // Peace
+            mode = new module.exports.Peaceful();
+            break;
+        case 7: // Teleports
+            mode = new module.exports.Teleports();
+            break;
+        case 10: // Tournament
+            mode = new module.exports.Tournament();
+            break;
+        case 11: // Hunger Games
+            mode = new module.exports.HungerGames();
+            break;
+        case 12: // Zombie
+            mode = new module.exports.Zombie();
+            break;
+        case 13: // Zombie Team
+            mode = new module.exports.TeamZ();
+            break;
+        case 14: // Experimental Team
+            mode = new module.exports.TeamX();
+            break;
+        case 20: // Rainbow
+            mode = new module.exports.Rainbow();
+            break;
+        case 21: // Debug
+            mode = new module.exports.Debug();
+            break;
+        default: // FFA is default
+            mode = new module.exports.FFA();
+            break;
+    }
+    return mode;
+};
+
+module.exports.get = get;
